b31d46048d4e4d051ea186e76b9cd1eda73d9d77
Yudiz Solutions Pvt. Ltd <yudiz-bhoomit@Yudizs-Mac-mini.local>
2022-06-06 18:50:16 +0530

    Add: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const kycButton = document.getElementById('kyc');
+
+kycButton.addEventListener('click', (e) => {
+    console.log('button clicked');
+});
+
+setTimeout(() => {
+    const blockpass = new BlockpassKYCConnect('bfk_warzone_8b8bd', {
+        elementId: 'kyc',
+        mainColor: '800080',
+        email: 'yuvraj.u@yudiz.in',
+        refId: '620f42fcc8afd9e5041d635f',
+    });
+    blockpass.startKYCConnect()
+
+}, 100);
